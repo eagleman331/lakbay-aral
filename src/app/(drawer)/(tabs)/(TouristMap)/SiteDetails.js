0b8de5a0d5be4dac1584401a295defa44d2ed500
John Constantine <warrenching33@gmail.com>
2025-03-21 15:47:34 +0800

    Added Video Screen

--- src/app/(drawer)/(tabs)/(TouristMap)/SiteDetails.js
+++ src/app/(drawer)/(tabs)/(TouristMap)/SiteDetails.js
@@ -1,10 +1,16 @@
-import { View, Text, useWindowDimensions, FlatList, Image, Button } from 'react-native';
+import { View, Text, useWindowDimensions, FlatList, Image, Button, Pressable } from 'react-native';
 import React from 'react';
 import { router, useLocalSearchParams } from 'expo-router';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 const SiteDetails = () => {
   const { width, height } = useWindowDimensions();
   const params = useLocalSearchParams(); // Retrieves query params
+
+  const scale = useSharedValue(1);
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+  }));
   console.log('params', params);
   return (
     <>
@@ -32,10 +38,29 @@ const SiteDetails = () => {
             <Text className="font-bold">Aminities</Text>
             <Text className="ml-2 mt-2 font-light">{params.Aminities}</Text>
           </View>
+          <View className='flex-row ' style={{justifyContent:'space-around'}}>
+            <Pressable
+              onPressIn={() => (scale.value = withSpring(0.9, { damping: 5, stiffness: 150 }))} // Press effect
+              onPressOut={() => (scale.value = withSpring(1, { damping: 5, stiffness: 150 }))} // Release effect
+              onPress={() => router.push({ pathname: '/(drawer)/(tabs)/(TouristMap)/VideoScreen' })} // OnPress event
+            >
+              <Animated.View
+                className="w-1/5 rounded-2xl bg-red-500 justify-center"
+                style={{ animatedStyle, height: height * 0.12, width:width*.3 }}>
+                <Text className='text-center' >warren</Text>
+              </Animated.View>
+            </Pressable>
+            <View className='bg-slate-500' style={{justifyContent:'flex-end'}}>
+              <Text>Directions</Text>
+            </View>
+          </View>
+
           <Button
-            onPress={()=> router.push({
-              pathname: '/(drawer)/(tabs)/(TouristMap)/VideoScreen',
-            })} // OnPress event
+            onPress={() =>
+              router.push({
+                pathname: '/(drawer)/(tabs)/(TouristMap)/VideoScreen',
+              })
+            } // OnPress event
             title="Play Video"
           />
         </View>
